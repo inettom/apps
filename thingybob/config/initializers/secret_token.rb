c2676e5c9e64f1c057776f323e92ffc3872899bb
Tomas Markus <inettom@gmail.com>
2012-12-14 20:45:40 +0100

    Thingy bob test app commit

---
+++ thingybob/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thingybob::Application.config.secret_token = '41bfab192874bca05c177926ee9c08f454f628e2c7c84a3b181b72bdb4047ef9e31c6b2faf42544cf1e709e198463b755d0f73b98e668be51534f5af1bd2e782'
